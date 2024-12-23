415fd8f93cc5a25edceb6e6f3fee37db796bfe35
dzmitrysopat <dzmitrysopat@gmail.com>
2024-12-23 23:31:54 +0300

    main

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-if (document.querySelector('main article section .hw-block').hasAttribute("onclick")) {
-    console.log('have');
-}
-else{
-    console.log('no');
-    document.querySelector('main article section .hw-block').classList.add("gray");    
-}
+var attr = document.querySelector("section.topic-block > section.hw-block").getAttribute("onclick").length;
+
+console.log(attr);
+
+var kol = document.querySelectorAll("section.topic-block > section.hw-block").length;
+console.log(kol);
+
+// if (document.querySelector('hw-block').attributes) {
+//     console.log('have');
+//     document.querySelector('main article section .hw-block').classList.add("gray"); 
+// }
+// else{
+//     console.log('no');
+//     // document.querySelector('main article section .hw-block').classList.add("gray");    
+// }
 
