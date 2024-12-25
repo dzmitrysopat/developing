5199bb708077e25eb86be8a16b3cde9d860a9111
dzmitrysopat <dzmitrysopat@gmail.com>
2024-12-25 21:30:05 +0300

    add score scale and score numbers

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
+const progressContainer = document.querySelector('#progress');
+const bodyContainer = document.querySelector('.score')
 const attrs = document.querySelectorAll("section.topic-block > section.hw-block");
-// console.log(attrs);
+const progressTemplate = 
+`<progress max="%max%" value="%value%" id="progress"></progress>`;
+let value = 0;
+
+// let progress = document.getElementById("progress").getAttribute('max');
+
+progressContainer.max = attrs.length;
+
+// let kol = attrs.getAttribute('onclick').length == 0;
+// console.log(kol);
+
+// let value = document.getElementById("progress").getAttribute('value');
+// console.log(value);
 
 // var kol = document.querySelectorAll("section.topic-block > section.hw-block").length;
 // console.log(kol);
 
 for (const attr of attrs){
     if (attr.getAttribute('onclick').length == 0){
-        attr.classList.add("gray");
-    }
-    // else{
-    //     attr.classList.remove("gray")
-    // }
+        attr.classList.add("gray");        
+        }
+    else{
+        value = value + 1;
+        // console.log(value);
+        progressContainer.value = value;
+    }    
 }
+
+// score in progress block
+const scoreTemplate = `<p class="score">%score%</p>`;
+
+let score = `${value}/${attrs.length}`;
+const questionNumber = scoreTemplate.replace('%score%', score);
+
+bodyContainer.innerHTML = questionNumber;
+
+
+
+
+
