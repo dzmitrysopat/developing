068a77abbae467faaadc452ce7c4a4c691abe9af
dzmitrysopat <102973988+dzmitrysopat@users.noreply.github.com>
2024-12-26 09:25:58 +0300

    del some lines in js

--- script.js
+++ script.js
@@ -5,18 +5,7 @@ const progressTemplate =
 `<progress max="%max%" value="%value%" id="progress"></progress>`;
 let value = 0;
 
-// let progress = document.getElementById("progress").getAttribute('max');
-
 progressContainer.max = attrs.length;
-
-// let kol = attrs.getAttribute('onclick').length == 0;
-// console.log(kol);
-
-// let value = document.getElementById("progress").getAttribute('value');
-// console.log(value);
-
-// var kol = document.querySelectorAll("section.topic-block > section.hw-block").length;
-// console.log(kol);
 
 for (const attr of attrs){
     if (attr.getAttribute('onclick').length == 0){
